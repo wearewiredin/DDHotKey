wearewiredin/DDHotKey

commit 375f0d9ce274bb324e6354427a3acc4aa34cd627

Rename demo app target to DDHotKeyDemo, fix deprecationw warnings

DDHotKeyTextField.m
@@ -82,9 +82,9 @@ static DDHotKeyTextFieldEditor *DDFieldEditor(void) {
 
 - (void)processHotkeyEvent:(NSEvent *)event {
     NSUInteger flags = event.modifierFlags;
-    BOOL hasModifier = (flags & (NSCommandKeyMask | NSAlternateKeyMask | NSControlKeyMask | NSShiftKeyMask | NSFunctionKeyMask)) > 0;
+    BOOL hasModifier = (flags & (NSEventModifierFlagCommand | NSEventModifierFlagOption | NSEventModifierFlagControl | NSEventModifierFlagShift | NSEventModifierFlagFunction)) > 0;
     
-    if (event.type == NSKeyDown) {
+    if (event.type == NSEventTypeKeyDown) {
         _hasSeenKeyDown = YES;
         unichar character = [event.charactersIgnoringModifiers characterAtIndex:0];
         
@@ -102,7 +102,7 @@ static DDHotKeyTextFieldEditor *DDFieldEditor(void) {
         }
     }
     
-    if ((event.type == NSKeyDown || (event.type == NSFlagsChanged && _hasSeenKeyDown == NO)) && hasModifier) {
+    if ((event.type == NSEventTypeKeyDown || (event.type == NSEventTypeFlagsChanged && _hasSeenKeyDown == NO)) && hasModifier) {
         self.hotKeyField.hotKey = [DDHotKey hotKeyWithKeyCode:event.keyCode modifierFlags:flags task:_originalHotKey.task];
         NSString *str = DDStringFromKeyCode(event.keyCode, flags);
         [self.textStorage.mutableString setString:[str uppercaseString]];
@@ -114,7 +114,7 @@ static DDHotKeyTextFieldEditor *DDFieldEditor(void) {
     BOOL ok = [super becomeFirstResponder];
     if (ok) {
         _hasSeenKeyDown = NO;
-        _globalMonitor = [NSEvent addLocalMonitorForEventsMatchingMask:(NSKeyDownMask | NSFlagsChangedMask) handler:^NSEvent*(NSEvent *event){
+        _globalMonitor = [NSEvent addLocalMonitorForEventsMatchingMask:(NSEventTypeKeyDown | NSEventTypeFlagsChanged) handler:^NSEvent*(NSEvent *event){
             [self processHotkeyEvent:event];
             return nil;
         }];

DDHotKey-Info.plist
@@ -9,21 +9,21 @@
 	<key>CFBundleIconFile</key>
 	<string></string>
 	<key>CFBundleIdentifier</key>
-	<string>com.yourcompany.${PRODUCT_NAME:rfc1034identifier}</string>
+	<string>$(PRODUCT_BUNDLE_IDENTIFIER)</string>
 	<key>CFBundleInfoDictionaryVersion</key>
 	<string>6.0</string>
 	<key>CFBundleName</key>
 	<string>${PRODUCT_NAME}</string>
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
-	<key>CFBundleSignature</key>
-	<string>????</string>
 	<key>CFBundleShortVersionString</key>
 	<string>1.0</string>
-	<key>LSMinimumSystemVersion</key>
-	<string>${MACOSX_DEPLOYMENT_TARGET}</string>
+	<key>CFBundleSignature</key>
+	<string>????</string>
 	<key>CFBundleVersion</key>
 	<string>1</string>
+	<key>LSMinimumSystemVersion</key>
+	<string>${MACOSX_DEPLOYMENT_TARGET}</string>
 	<key>NSMainNibFile</key>
 	<string>MainMenu</string>
 	<key>NSPrincipalClass</key>

DDHotKey.xcodeproj/project.pbxproj
@@ -25,7 +25,6 @@
 		1DDD58150DA1D0A300B32029 /* English */ = {isa = PBXFileReference; lastKnownFileType = file.xib; name = English; path = English.lproj/MainMenu.xib; sourceTree = "<group>"; };
 		256AC3D80F4B6AC300CF3369 /* DDHotKeyAppDelegate.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = DDHotKeyAppDelegate.h; sourceTree = "<group>"; };
 		256AC3D90F4B6AC300CF3369 /* DDHotKeyAppDelegate.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = DDHotKeyAppDelegate.m; sourceTree = "<group>"; };
-		256AC3F00F4B6AF500CF3369 /* DDHotKey_Prefix.pch */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = DDHotKey_Prefix.pch; sourceTree = "<group>"; };
 		29B97316FDCFA39411CA2CEA /* main.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = main.m; sourceTree = "<group>"; };
 		29B97324FDCFA39411CA2CEA /* AppKit.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = AppKit.framework; path = /System/Library/Frameworks/AppKit.framework; sourceTree = "<absolute>"; };
 		29B97325FDCFA39411CA2CEA /* Foundation.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = Foundation.framework; path = /System/Library/Frameworks/Foundation.framework; sourceTree = "<absolute>"; };
@@ -57,8 +56,7 @@
 			isa = PBXGroup;
 			children = (
 				55CD8D55181C2F9E003799D0 /* DDHotKey */,
-				256AC3D80F4B6AC300CF3369 /* DDHotKeyAppDelegate.h */,
-				256AC3D90F4B6AC300CF3369 /* DDHotKeyAppDelegate.m */,
+				9DA121262066F7E6002D557D /* DDHotKeyDemo */,
 			);
 			name = Classes;
 			sourceTree = "<group>";
@@ -105,7 +103,6 @@
 		29B97315FDCFA39411CA2CEA /* Other Sources */ = {
 			isa = PBXGroup;
 			children = (
-				256AC3F00F4B6AF500CF3369 /* DDHotKey_Prefix.pch */,
 				29B97316FDCFA39411CA2CEA /* main.m */,
 			);
 			name = "Other Sources";
@@ -143,12 +140,21 @@
 			name = DDHotKey;
 			sourceTree = "<group>";
 		};
+		9DA121262066F7E6002D557D /* DDHotKeyDemo */ = {
+			isa = PBXGroup;
+			children = (
+				256AC3D80F4B6AC300CF3369 /* DDHotKeyAppDelegate.h */,
+				256AC3D90F4B6AC300CF3369 /* DDHotKeyAppDelegate.m */,
+			);
+			name = DDHotKeyDemo;
+			sourceTree = "<group>";
+		};
 /* End PBXGroup section */
 
 /* Begin PBXNativeTarget section */
-		8D1107260486CEB800E47090 /* DDHotKey */ = {
+		8D1107260486CEB800E47090 /* DDHotKeyDemo */ = {
 			isa = PBXNativeTarget;
-			buildConfigurationList = C01FCF4A08A954540054247B /* Build configuration list for PBXNativeTarget "DDHotKey" */;
+			buildConfigurationList = C01FCF4A08A954540054247B /* Build configuration list for PBXNativeTarget "DDHotKeyDemo" */;
 			buildPhases = (
 				8D1107290486CEB800E47090 /* Resources */,
 				8D11072C0486CEB800E47090 /* Sources */,
@@ -158,7 +164,7 @@
 			);
 			dependencies = (
 			);
-			name = DDHotKey;
+			name = DDHotKeyDemo;
 			productInstallPath = "$(HOME)/Applications";
 			productName = DDHotKey;
 			productReference = 8D1107320486CEB800E47090 /* DDHotKey.app */;
@@ -186,7 +192,7 @@
 			projectDirPath = "";
 			projectRoot = "";
 			targets = (
-				8D1107260486CEB800E47090 /* DDHotKey */,
+				8D1107260486CEB800E47090 /* DDHotKeyDemo */,
 			);
 		};
 /* End PBXProject section */
@@ -247,10 +253,9 @@
 				GCC_DYNAMIC_NO_PIC = NO;
 				GCC_MODEL_TUNING = G5;
 				GCC_OPTIMIZATION_LEVEL = 0;
-				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = DDHotKey_Prefix.pch;
 				INFOPLIST_FILE = "DDHotKey-Info.plist";
 				INSTALL_PATH = "$(HOME)/Applications";
+				PRODUCT_BUNDLE_IDENTIFIER = com.DaveDeLong.DDHotKeyDemo;
 				PRODUCT_NAME = DDHotKey;
 			};
 			name = Debug;
@@ -262,10 +267,9 @@
 				COMBINE_HIDPI_IMAGES = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				GCC_MODEL_TUNING = G5;
-				GCC_PRECOMPILE_PREFIX_HEADER = YES;
-				GCC_PREFIX_HEADER = DDHotKey_Prefix.pch;
 				INFOPLIST_FILE = "DDHotKey-Info.plist";
 				INSTALL_PATH = "$(HOME)/Applications";
+				PRODUCT_BUNDLE_IDENTIFIER = com.DaveDeLong.DDHotKeyDemo;
 				PRODUCT_NAME = DDHotKey;
 			};
 			name = Release;
@@ -288,6 +292,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES;
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
+				MACOSX_DEPLOYMENT_TARGET = 10.12;
 				ONLY_ACTIVE_ARCH = YES;
 				SDKROOT = macosx;
 			};
@@ -310,6 +315,7 @@
 				GCC_WARN_UNINITIALIZED_AUTOS = YES;
 				GCC_WARN_UNUSED_FUNCTION = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
+				MACOSX_DEPLOYMENT_TARGET = 10.12;
 				SDKROOT = macosx;
 			};
 			name = Release;
@@ -317,7 +323,7 @@
 /* End XCBuildConfiguration section */
 
 /* Begin XCConfigurationList section */
-		C01FCF4A08A954540054247B /* Build configuration list for PBXNativeTarget "DDHotKey" */ = {
+		C01FCF4A08A954540054247B /* Build configuration list for PBXNativeTarget "DDHotKeyDemo" */ = {
 			isa = XCConfigurationList;
 			buildConfigurations = (
 				C01FCF4B08A954540054247B /* Debug */,

DDHotKey.xcodeproj/xcuserdata/andrewmadsen.xcuserdatad/xcschemes/xcschememanagement.plist
@@ -1,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>SchemeUserState</key>
+	<dict>
+		<key>DDHotKeyDemo.xcscheme</key>
+		<dict>
+			<key>orderHint</key>
+			<integer>0</integer>
+		</dict>
+	</dict>
+	<key>SuppressBuildableAutocreation</key>
+	<dict/>
+</dict>
+</plist>

DDHotKeyAppDelegate.m
@@ -40,7 +40,7 @@
 - (IBAction) registerExample1:(id)sender {
 	[self addOutput:@"Attempting to register hotkey for example 1"];
 	DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
-	if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:NSControlKeyMask target:self action:@selector(hotkeyWithEvent:) object:nil]) {
+	if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:NSEventModifierFlagControl target:self action:@selector(hotkeyWithEvent:) object:nil]) {
 		[self addOutput:@"Unable to register hotkey for example 1"];
 	} else {
 		[self addOutput:@"Registered hotkey for example 1"];
@@ -51,7 +51,7 @@
 - (IBAction) registerExample2:(id)sender {
 	[self addOutput:@"Attempting to register hotkey for example 2"];
 	DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
-	if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSControlKeyMask | NSAlternateKeyMask) target:self action:@selector(hotkeyWithEvent:object:) object:@"hello, world!"]) {
+	if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSEventModifierFlagControl | NSEventModifierFlagOption) target:self action:@selector(hotkeyWithEvent:object:) object:@"hello, world!"]) {
 		[self addOutput:@"Unable to register hotkey for example 2"];
 	} else {
 		[self addOutput:@"Registered hotkey for example 2"];
@@ -69,7 +69,7 @@
 		[self addOutput:[NSString stringWithFormat:@"Hotkey event: %@", hkEvent]];
 		[self addOutput:[NSString stringWithFormat:@"the answer is: %d", theAnswer]];	
 	};
-	if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask) task:task]) {
+	if (![c registerHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSEventModifierFlagControl | NSEventModifierFlagOption | NSEventModifierFlagCommand) task:task]) {
 		[self addOutput:@"Unable to register hotkey for example 3"];
 	} else {
 		[self addOutput:@"Registered hotkey for example 3"];
@@ -82,19 +82,19 @@
 
 - (IBAction) unregisterExample1:(id)sender {
 	DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
-	[c unregisterHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:NSControlKeyMask];
+	[c unregisterHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:NSEventModifierFlagControl];
 	[self addOutput:@"Unregistered hotkey for example 1"];
 }
 
 - (IBAction) unregisterExample2:(id)sender {
 	DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
-	[c unregisterHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSControlKeyMask | NSAlternateKeyMask)];
+	[c unregisterHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSEventModifierFlagControl | NSEventModifierFlagOption)];
 	[self addOutput:@"Unregistered hotkey for example 2"];
 }
 
 - (IBAction) unregisterExample3:(id)sender {
 	DDHotKeyCenter *c = [DDHotKeyCenter sharedHotKeyCenter];
-	[c unregisterHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask)];
+	[c unregisterHotKeyWithKeyCode:kVK_ANSI_V modifierFlags:(NSEventModifierFlagControl | NSEventModifierFlagOption | NSEventModifierFlagCommand)];
 	[self addOutput:@"Unregistered hotkey for example 3"];
 }
 

DDHotKeyCenter.h
@@ -41,14 +41,14 @@ typedef void (^DDHotKeyTask)(NSEvent*);
 
 /**
  Register a target/action hotkey.
- The modifierFlags must be a bitwise OR of NSCommandKeyMask, NSAlternateKeyMask, NSControlKeyMask, or NSShiftKeyMask;
+ The modifierFlags must be a bitwise OR of NSEventModifierFlagCommand, NSEventModifierFlagOption, NSEventModifierFlagControl, or NSEventModifierFlagShift;
  Returns the hotkey registered.  If registration failed, returns nil.
  */
 - (DDHotKey *)registerHotKeyWithKeyCode:(unsigned short)keyCode modifierFlags:(NSUInteger)flags target:(id)target action:(SEL)action object:(id)object;
 
 /**
  Register a block callback hotkey.
- The modifierFlags must be a bitwise OR of NSCommandKeyMask, NSAlternateKeyMask, NSControlKeyMask, or NSShiftKeyMask;
+ The modifierFlags must be a bitwise OR of NSEventModifierFlagCommand, NSEventModifierFlagOption, NSEventModifierFlagControl, or NSEventModifierFlagShift;
  Returns the hotkey registered.  If registration failed, returns nil.
  */
 - (DDHotKey *)registerHotKeyWithKeyCode:(unsigned short)keyCode modifierFlags:(NSUInteger)flags task:(DDHotKeyTask)task;

DDHotKeyCenter.m
@@ -65,10 +65,10 @@ OSStatus dd_hotKeyHandler(EventHandlerCallRef nextHandler, EventRef theEvent, vo
 
 - (NSString *)description {
     NSMutableArray *bits = [NSMutableArray array];
-    if ((_modifierFlags & NSControlKeyMask) > 0) { [bits addObject:@"NSControlKeyMask"]; }
-    if ((_modifierFlags & NSCommandKeyMask) > 0) { [bits addObject:@"NSCommandKeyMask"]; }
-    if ((_modifierFlags & NSShiftKeyMask) > 0) { [bits addObject:@"NSShiftKeyMask"]; }
-    if ((_modifierFlags & NSAlternateKeyMask) > 0) { [bits addObject:@"NSAlternateKeyMask"]; }
+    if ((_modifierFlags & NSEventModifierFlagControl) > 0) { [bits addObject:@"NSEventModifierFlagControl"]; }
+    if ((_modifierFlags & NSEventModifierFlagCommand) > 0) { [bits addObject:@"NSEventModifierFlagCommand"]; }
+    if ((_modifierFlags & NSEventModifierFlagShift) > 0) { [bits addObject:@"NSEventModifierFlagShift"]; }
+    if ((_modifierFlags & NSEventModifierFlagOption) > 0) { [bits addObject:@"NSEventModifierFlagOption"]; }
     
     NSString *flags = [NSString stringWithFormat:@"(%@)", [bits componentsJoinedByString:@" | "]];
     NSString *invokes = @"(block)";
@@ -266,7 +266,7 @@ OSStatus dd_hotKeyHandler(EventHandlerCallRef nextHandler, EventRef theEvent, vo
         DDHotKey *matchingHotKey = [matchingHotKeys anyObject];
         
         NSEvent *event = [NSEvent eventWithEventRef:theEvent];
-        NSEvent *keyEvent = [NSEvent keyEventWithType:NSKeyUp
+        NSEvent *keyEvent = [NSEvent keyEventWithType:NSEventTypeKeyUp
                                              location:[event locationInWindow]
                                         modifierFlags:[event modifierFlags]
                                             timestamp:[event timestamp]

commit 77093d4b4699d105862a82af2520bde38b49fde6

Fix bug in DDHotKeyTextField (introduced in 375f0d9ce274)

DDHotKeyTextField.m
@@ -114,7 +114,7 @@ static DDHotKeyTextFieldEditor *DDFieldEditor(void) {
     BOOL ok = [super becomeFirstResponder];
     if (ok) {
         _hasSeenKeyDown = NO;
-        _globalMonitor = [NSEvent addLocalMonitorForEventsMatchingMask:(NSEventTypeKeyDown | NSEventTypeFlagsChanged) handler:^NSEvent*(NSEvent *event){
+        _globalMonitor = [NSEvent addLocalMonitorForEventsMatchingMask:(NSEventMaskKeyDown | NSEventMaskFlagsChanged) handler:^NSEvent*(NSEvent *event){
             [self processHotkeyEvent:event];
             return nil;
         }];